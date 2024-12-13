28f7280626a4c4f242bc8e1242422f96a28e22c8
braydenhamilton <bham873@aucklanduni.ac.nz>
2024-12-13 15:02:41 +1300

    setup booking repository

--- tee-time-core/src/main/java/com/golf/teetimecore/repository/BookingRepository.java
+++ tee-time-core/src/main/java/com/golf/teetimecore/repository/BookingRepository.java
@@ -1,4 +1,8 @@
 package com.golf.teetimecore.repository;
 
-public class BookingRepository {
-}
+import com.golf.teetimecore.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {}
