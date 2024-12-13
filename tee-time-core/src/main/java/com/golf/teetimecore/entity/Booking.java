3b768e92207c86c40ae3caa17328de8141aeef50
braydenhamilton <bham873@aucklanduni.ac.nz>
2024-12-13 15:13:15 +1300

    set up booking entity

--- tee-time-core/src/main/java/com/golf/teetimecore/entity/Booking.java
+++ tee-time-core/src/main/java/com/golf/teetimecore/entity/Booking.java
@@ -1,4 +1,24 @@
 package com.golf.teetimecore.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime teeTime;
+
+    @ElementCollection
+    private List<String> members;
 }
