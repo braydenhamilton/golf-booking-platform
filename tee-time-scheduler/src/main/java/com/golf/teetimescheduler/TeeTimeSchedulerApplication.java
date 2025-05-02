0e33d194418de61e9ffc15cd922113793832b5de
braydenhamilton <bham873@aucklanduni.ac.nz>
2025-05-03 11:31:04 +1200

    tee time scheduler init

---
+++ tee-time-scheduler/src/main/java/com/golf/teetimescheduler/TeeTimeSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.golf.teetimescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeeTimeSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeeTimeSchedulerApplication.class, args);
+	}
+
+}
